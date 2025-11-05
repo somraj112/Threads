9d8f062ee2cff48fa066ce52d00d7c5f10b3bd2e
somraj112 <somrajnandi112@gmail.com>
2025-11-05 23:56:11 +0530

    post page done

--- client/src/components/home/Post.jsx
+++ client/src/components/home/Post.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Stack, Typography } from "@mui/material";
+import { IoIosMore } from "react-icons/io";
+import React from "react";
+import PostOne from "./post/PostOne";
+import PostTwo from "./post/postTwo";
 
 const Post = () => {
   return (
-    <>Post comp</>
-  )
-}
+    <>
+    <Stack
+      flexDirection={"row"}
+      justifyContent={"space-between"}
+      borderBottom={"3px solid gray"}
+      p={2}
+      mx={"auto"}
+      width={"70%"}
+      sx={{
+        ":hover": {
+          cursor: "pointer",
+          boxShadow: "10px 10px 10px gray",
+        },
+        transition: "all ease-in-out 0.3s",
+      }}
+    >
+      <Stack flexDirection={"row"} gap={2}>
+        <PostOne />
+        <PostTwo />
+      </Stack>
+      <Stack
+        flexDirection={"row"}
+        justifyContent={"center"}
+        gap={1}
+        fontSize={"1rem"}
+      >
+        <Typography
+          variant="caption"
+          color={"GrayText"}
+          fontSize={"1rem"}
+          position={"relative"}
+          top={2}
+        >
+          24h
+        </Typography>
+          <IoIosMore size={28}/>
+      </Stack>
+    </Stack>
+  </>
+  );
+};
 
-export default Post
+export default Post;
